24974dddcd0baae21326f3614991cefec13373e9
Shashwat Kumar <thegreatshasha@gmail.com>
2015-10-29 02:27:05 +0530

    add deepmind paper for convenience

--- test.py
+++ test.py
@@ -16,11 +16,29 @@ ale.loadROM('/Users/shashwat/Downloads/space_invaders.bin')
 legal_actions = ale.getMinimalActionSet()
 
 # Run actions over each episode randomly and print reward
+
+# How to get screen rgb?
+width, height = ale.getScreenDims()
+screen_buffer = np.empty((height, width), dtype=np.uint8)
+
+# Initialize a neural network according to nature paper
+
+# Version1, let it grow, let it grow
 for episode in range(10):
     total_reward = 0.0
+    
+    # Replace this with fixed time (we can't have loopy episodes)
     while not ale.game_over():
+
+        # Select action from neural net output instead of random weights
+
         a = legal_actions[np.random.randint(legal_actions.size)]
         reward = ale.act(a);
         total_reward += reward
+
+        # Get buffer data and store it in screen_buffer
+        ale.getScreenGrayscale(screen_buffer)
+        import pdb; pdb.set_trace()
+
     print("Episode " + str(episode) + " ended with score: " + str(total_reward))
     ale.reset_game()
