64e84c8932fc80125bb506ca578f8205f3dd8ea8
Shashwat Kumar <thegreatshasha@gmail.com>
2015-11-03 16:49:14 +0530

    add feedforward code

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def resize_image(image, width, height, resized_width, resized_height, crop_offset, resize_method = 'crop'):
         """ Appropriately resize a single image """
@@ -20,3 +21,16 @@ def resize_image(image, width, height, resized_width, resized_height, crop_offse
         
         else:
             raise ValueError('Unrecognized image resize method.')
+
+def get_processed_screen(ale):
+    RESIZE_METHOD = 'crop'
+    RESIZED_WIDTH = 84
+    RESIZED_HEIGHT = 84
+    CROP_OFFSET = 8
+    width, height = ale.getScreenDims()
+    screen_buffer = np.empty((height, width), dtype=np.uint8)
+    
+    ale.getScreenGrayscale(screen_buffer)
+
+    resized_image = resize_image(screen_buffer, width, height, RESIZED_WIDTH, RESIZED_HEIGHT, CROP_OFFSET, resize_method = 'crop')
+    return resized_image
--- test.py
+++ test.py
@@ -1,20 +1,18 @@
 from ale_python_interface import ALEInterface
 import numpy as np
 import pygame
-from helpers import resize_image
+from helpers import get_processed_screen
 import keras
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten
 from keras.layers.convolutional import Convolution2D
+from keras.optimizers import SGD
 
 # Create the ale interface and load rom files
 ale = ALEInterface()
 
 # Some common settings
-RESIZE_METHOD = 'crop'
-RESIZED_WIDTH = 84
-RESIZED_HEIGHT = 84
-CROP_OFFSET = 8
+HISTORY_LENGTH = 4
 
 # Comment this to disable gui
 ale.setBool('display_screen', True)
@@ -46,7 +44,9 @@ model.add(Flatten())
 model.add(Dense(256))
 model.add(Activation('relu'))
 model.add(Dense(legal_actions.shape[0]))
-import pdb; pdb.set_trace()
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='mean_squared_error', optimizer=sgd)
+## will SGD work with multiple function calls?
 
 # Version1, let it grow, let it grow
 for episode in range(10):
@@ -55,18 +55,20 @@ for episode in range(10):
     # Replace this with fixed time (we can't have loopy episodes)
     while not ale.game_over():
 
-        # Select action from neural net output instead of random weights
-
-        a = legal_actions[np.random.randint(legal_actions.size)]
-        reward = ale.act(a);
+        # get best possible action from the current neural network
+        image = get_processed_screen(ale)
+        history = np.array([image]*HISTORY_LENGTH)
+        history_batch = np.array([history])
+        prediction = model.predict(history_batch)[0]
+        best_action = legal_actions[np.argmax(prediction)]
+        
+        # act on the best possible action
+        reward = ale.act(best_action);
         total_reward += reward
 
-        # Get buffer data and store it in screen_buffer
-        ale.getScreenGrayscale(screen_buffer)
+        #if total_reward != 0:
+        #import pdb; pdb.set_trace()
 
-        awesome_image = resize_image(screen_buffer, width, height, RESIZED_WIDTH, RESIZED_HEIGHT, CROP_OFFSET, resize_method = 'crop')
-        images.append(awesome_image)
-        import pdb; pdb.set_trace()
 
     print("Episode " + str(episode) + " ended with score: " + str(total_reward))
     ale.reset_game()
